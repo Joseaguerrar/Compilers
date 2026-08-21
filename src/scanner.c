2d420133ab4a1d715081805e6a20929a97c1848c
Jose Guerra <joseaguerrar2017@gmail.com>
2026-08-20 23:07:18 -0600

    Scanner cycle implemented considering only whitespaces

--- src/scanner.c
+++ src/scanner.c
@@ -5,5 +5,17 @@
 
 Token scanner(void)
 {
+    int in_char;
+
+    // Read input while characters are available
+    while ((in_char = getchar()) != EOF) {
+        // Skip whitespace characters
+        if (isspace(in_char))
+        {
+            continue;
+        }
+       
+    }
+    
     return SCANEOF;
 }
