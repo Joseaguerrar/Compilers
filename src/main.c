31f66991cc09234b7ac611cfe3106733490f4edc
Jose Guerra <joseaguerrar2017@gmail.com>
2026-08-21 16:15:15 -0600

    Main with integrated tests for single character tokens

--- src/main.c
+++ src/main.c
@@ -4,11 +4,47 @@
 
 int main(void)
 {
-    Token token = scanner();
+    Token token;
 
-    if (token == SCANEOF) {
-        printf("SCANEOF\n");
-    }
+    do {
+        token = scanner();
+
+        switch (token) {
+
+            case INTLITERAL:
+                printf("INTLITERAL\n");
+                break;
+
+            case LPAREN:
+                printf("LPAREN\n");
+                break;
+
+            case RPAREN:
+                printf("RPAREN\n");
+                break;
+
+            case SEMICOLON:
+                printf("SEMICOLON\n");
+                break;
+
+            case COMMA:
+                printf("COMMA\n");
+                break;
+
+            case PLUSOP:
+                printf("PLUSOP\n");
+                break;
+
+            case SCANEOF:
+                printf("SCANEOF\n");
+                break;
+
+            default:
+                printf("UNKNOWN TOKEN\n");
+                break;
+        }
+
+    } while (token != SCANEOF);
 
     return 0;
 }
