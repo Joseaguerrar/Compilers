4e6cbb1092df23614d2cde4be893d5a52ae8d2a3
MarioCordero <mariogabriel.cordero@ucr.ac.cr>
2026-08-21 08:23:21 -0600

    # FEAT:
    - Added code based on the Fischer Book for the scanner

--- src/scanner.c
+++ src/scanner.c
@@ -19,3 +19,39 @@ Token scanner(void)
     
     return SCANEOF;
 }
+
+// Book code:
+//
+// #include <stdio.h>
+// #include <ctype.h>
+
+// int in_char, c;
+
+// while ((in_char = getchar()) != EOF) {
+//     if (isspace(in_char))
+//         continue;       /* do nothing */
+//     else if (isalpha(in_char)) {
+//         /*
+//          * ID ::= LETTER | ID LETTER
+//          *               | ID DIGIT
+//          *               | ID UNDERSCORE
+//          */
+//         for (c = getchar(); isalnum(c) || c == '_';
+//                 c = getchar())
+//             ;
+//         ungetc(c, stdin);
+
+//         return ID;
+//     } else if (isdigit(in_char)) {
+//         /*
+//          * INTLITERAL ::= DIGIT |
+//          *                INTLITERAL DIGIT
+//          */
+//         while (isdigit((c = getchar())))
+//             ;
+//         ungetc(c, stdin);
+
+//         return INTLITERAL;
+//     } else
+//         lexical_error(in_char);
+// }
