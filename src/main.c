7c14a0544e604c236cad67aefd3ccc01d9ae9e70
Jose Guerra <joseaguerrar2017@gmail.com>
2026-08-21 23:49:25 -0600

    new logic in main for different tests through console

--- src/main.c
+++ src/main.c
@@ -1,76 +1,7 @@
-#include <stdio.h>
-
-#include "scanner.h"
-
-extern char token_buffer[];
+#include "parser.h"
 
 int main(void)
 {
-    Token token;
-
-    do {
-        token = scanner();
-
-        switch (token) {
-
-            case BEGIN:
-                printf("BEGIN\n");
-                break;
-
-            case END:
-                printf("END\n");
-                break;
-
-            case READ:
-                printf("READ\n");
-                break;
-
-            case WRITE:
-                printf("WRITE\n");
-                break;
-
-            case ID:
-                printf("ID: %s\n", token_buffer);
-                break;
-
-            case INTLITERAL:
-                printf("INTLITERAL: %s\n", token_buffer);
-                break;
-
-            case LPAREN:
-                printf("LPAREN\n");
-                break;
-
-            case RPAREN:
-                printf("RPAREN\n");
-                break;
-
-            case SEMICOLON:
-                printf("SEMICOLON\n");
-                break;
-
-            case COMMA:
-                printf("COMMA\n");
-                break;
-
-            case ASSIGNOP:
-                printf("ASSIGNOP\n");
-                break;
-
-            case PLUSOP:
-                printf("PLUSOP\n");
-                break;
-
-            case MINUSOP:
-                printf("MINUSOP\n");
-                break;
-
-            case SCANEOF:
-                printf("SCANEOF\n");
-                break;
-        }
-
-    } while (token != SCANEOF);
-
+    system_goal();
     return 0;
 }
