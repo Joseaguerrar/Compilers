96ac0cccad7718adbd2681498fc75ecf66c8b580
Jose Guerra <joseaguerrar2017@gmail.com>
2026-08-22 14:56:05 -0600

    Semantic header with struct definitions for expressions and operators records

---
+++ include/semantic.h
@@ -0,0 +1,24 @@
+#ifndef SEMANTIC_H
+#define SEMANTIC_H
+
+#define MAXIDLEN 33
+typedef char string[MAXIDLEN];
+
+typedef struct operator { // For operators
+    enum op { PLUS, MINUS } operator;
+} op_rec;
+
+// Expression types
+enum expr { IDEXPR, LITERALEXPR, TEMPEXPR };
+
+// For primary and expression
+typedef struct expression {
+    enum expr kind;
+    union {
+        string name; // For IDEXPR, TEMPEXPR
+        int val; // For LITERALEXPR
+    };
+    
+} expr_rec;
+
+#endif
