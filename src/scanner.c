ef45da36e645200ad15cc388452c045f9c1ec1c4
Jose Guerra <joseaguerrar2017@gmail.com>
2026-08-25 09:29:18 -0600

    change buffer declaration

--- src/scanner.c
+++ src/scanner.c
@@ -6,7 +6,7 @@
 
 #define BUFFER_SIZE 33
 
-extern char token_buffer[BUFFER_SIZE];
+char token_buffer[BUFFER_SIZE];
 static int buffer_index = 0;
 
 // Clear the token_buffer and reset the buffer_index
