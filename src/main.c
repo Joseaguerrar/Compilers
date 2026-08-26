05e5c39a223a5e4044dd1e30b00216e7de2236d0
Jose Guerra <joseaguerrar2017@gmail.com>
2026-08-25 19:55:06 -0600

    adjusted main for complete tests cycle

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
+#include <stdio.h>
+
 #include "parser.h"
 
-int main(void)
+// Opens the Micro source file and starts the compilation.
+int main(int argc, char *argv[])
 {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <source.micro>\n", argv[0]);
+        return 1;
+    }
+
+    if (freopen(argv[1], "r", stdin) == NULL) {
+        perror("Error opening source file");
+        return 1;
+    }
+
     system_goal();
+
     return 0;
 }
