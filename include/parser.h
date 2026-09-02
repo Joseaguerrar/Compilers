9b9c9b0afbfb04b33c68410e66e803beef3bbb24
Jose Guerra <joseaguerrar2017@gmail.com>
2026-09-02 15:21:04 -0600

    cahnge system goal type

--- include/parser.h
+++ include/parser.h
@@ -19,11 +19,9 @@ extern Token current_token;
 /**
  * @brief Parses a complete Micro program and translates it to assembly code.
  *
- * This function implements the goal symbol of the grammar: it parses the
- * program body, verifies that the input ends immediately afterwards, and
- * invokes the semantic routines that emit the start, the body, and the
- * end of the target program.
+ * @return 0 if the source program is valid, or 1 if a lexical or syntax
+ *         error is detected.
  */
-void system_goal(void);
+int system_goal(void);
 
 #endif
