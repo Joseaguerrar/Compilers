dc8bae4ef930e40520b59a6350623bec9a056b7e
24danielcr <24danielcr@gmail.com>
2026-09-09 16:28:34 -0600

    Deals with line comments and block comments

--- scanner/src/main.c
+++ scanner/src/main.c
@@ -1,10 +1,26 @@
 #define MAX 1
 #define MIN int
+#define PRO "MAMA"
+   //   s s sa  sa sa sa sssaaa a a a a a
+
+/*asas as as a /* askaskk as 
+
+askapsk pak psakp kap pdak */
+
+/*
+asaks
+
+saks
+as*/
+
+#include <stdio.h>
 
 int main() {
     int count = MAX;
     MIN less = MAX -2;
     double cc = 1.2;
+
+    printf("%s", PRO);
 
     return 0;
 }
