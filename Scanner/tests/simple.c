f74c9b9aa54c45832d56cce5039322d239ffa45e
Jose Guerra <joseaguerrar2017@gmail.com>
2026-09-16 20:34:28 -0600

    simple test

---
+++ Scanner/tests/simple.c
@@ -0,0 +1,13 @@
+int main(void)
+{
+    int number = 10;
+    double result = number + 3.14;
+    char letter = 'A';
+    char *message = "hola";
+
+    if (number >= 5) {
+        result = result + 1.0;
+    }
+
+    return number;
+}
